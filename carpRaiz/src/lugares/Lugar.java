7b2fca2cfe6b25603acd18b59069d1fa3cd3bdf3
Veranono29 <Veranono29@users.noreply.github.com>
2020-12-01 18:41:20 +0100

    G2Piza sigue haciendo cosas

---
+++ carpRaiz/src/lugares/Lugar.java
@@ -0,0 +1,9 @@
+package lugares;
+
+public class Lugar {
+
+	public Lugar() {
+		// TODO Auto-generated constructor stub
+	}
+//IDK tu sabrás jajajaja
+}
---
+++ carpRaiz/src/utensilios/Objetos.java
@@ -0,0 +1,9 @@
+package utensilios;
+
+public class Objetos {
+int ID;	//esto es el ID del objeto (sí, muy de C)
+	public Objetos() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

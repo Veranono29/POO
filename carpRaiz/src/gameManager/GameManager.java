7fd24b23d7955e26618844312e1839baa1037101
Veranono29 <Veranono29@users.noreply.github.com>
2020-12-29 19:02:25 +0100

    Pruebillas

--- carpRaiz/src/gameManager/GameManager.java
+++ carpRaiz/src/gameManager/GameManager.java
@@ -1,9 +1,19 @@
 package gameManager;
 
 public class GameManager {
-
+	//bucle es la variable con la que sabemos si han hecho algo en esta ronda
+	boolean bucle=true;
+	
 	public GameManager() {
 		// TODO Auto-generated constructor stub
 	}
-
+	/*
+	//COsas que hay qeu traducir
+while(bucle){
+	bucle = false;
+	//if(hacenCosas()&& !bucle){
+		//bucle =true;
+	//}
+}
+*/
 }
--- carpRaiz/src/jugadores/NNPC.java
+++ carpRaiz/src/jugadores/NNPC.java
@@ -1,7 +1,13 @@
 package jugadores;
 
+//Clase padre de ambos jugadores
 public abstract class NNPC {
-
+	//el objeto que tiene cada persona en posesión
+	//Cosa objeto = null; 
+	
+	//boolean de saber si han hecho algo
+	//boolean accion = false;
+	
 	public NNPC() {
 		// TODO Auto-generated constructor stub
 	}

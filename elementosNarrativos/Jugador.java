d2a899b76448c13dc6e51c396d011a33e4a1fe54
Reitz <raul_carrascal@hotmail.com>
2021-01-08 17:38:58 +0100

    Jugador completado

--- elementosNarrativos/Jugador.java
+++ elementosNarrativos/Jugador.java
@@ -1,27 +1,30 @@
 package elementosNarrativos;
 
-public class Jugador extends Agente {
-	//TODO El jugardor tambien tiene nombre. El game manager debera pedirlo de antes de ponerse a crear los Agentes, asique lo pedira antes de iniciar partida en pantalla emergente.
+import datos.Peticion;
+
+public abstract class Jugador extends Agente {
 	public Jugador(String nombre) {
 		super(nombre);
 	}
 	
-	@Override
-	public boolean dameAccion() {
-		// TODO dameAccion de jugador jugable
-		//if relocalizar
+	public boolean dameAccionJugador(Agente agente, Objeto objeto, Lugar lugar, int naccion) {
+		if(naccion == 0) {
+			Acciones.relocalizar(this, lugar);
 			return gestionJuego.GameManager.log(this, null, this.getLugar());
-			Acciones.relocalizar(this, /*LO QUE DIGA EL BOTON*/);
-		//if cojemos objeto
+		}
+		if(naccion == 1) {
 			Acciones.cogerObjeto(this, this.getLugar().getObjeto());
 			return gestionJuego.GameManager.log(this, this.getObjeto(), this.getLugar());
-		//if pedimos objeto
-			Acciones.pedirObjeto(/*LO QUE DIGA EL BOTON*/, new Peticion(this, this.getObjetivo().getObjeto()))
+		}
+		if(naccion == 2) {
+			Acciones.pedirObjeto(agente, new Peticion(this, this.getObjetivo().getObjeto()));
 			return gestionJuego.GameManager.log(this.getPeticion().getAgente(), this.getPeticion().getObjeto(), this.getPeticion().getLugar()); 
-		// if tenemos peticion
+		}
+		if(naccion == 3) {
 			Acciones.darObjeto(this);
 			return gestionJuego.GameManager.log(this.getPeticion().getAgente(), this.getPeticion().getObjeto(), this.getPeticion().getLugar());
-		//if nada
-			return gestionJuego.GameManager.log(this);
+		}
+		return gestionJuego.GameManager.log(this);
 	}
+
 }

7089197e3e6a6c8cc36df001b2bac4d4f468688a
Ekaitz723 <52826804+Ekaitz723@users.noreply.github.com>
2021-01-10 20:16:07 +0100

    Medio-hacer Jugador

--- elementosNarrativos/Jugador.java
+++ elementosNarrativos/Jugador.java
@@ -2,12 +2,32 @@ package elementosNarrativos;
 
 import datos.Peticion;
 
-public abstract class Jugador extends Agente {
+public class Jugador extends Agente {
 	public Jugador(String nombre) {
 		super(nombre);
 	}
 	
-	public boolean dameAccionJugador(Agente agente, Objeto objeto, Lugar lugar, int naccion) {
+	public boolean dameAccion(BotonDeMoverse boton) {
+		
+	}
+	
+	public boolean dameAccion(BotonDeCoger boton) {
+		
+	}
+	
+	public boolean dameAccion(BotonDePersona boton) {
+		//Aca
+	}
+	
+	public boolean dameAccion(boolean dar) {
+		if(dar) {
+			//Aca
+		}
+	}
+	
+	//TODO mete cada Acciones.relocalizar y return gestionJugo.GameManager, donde pone "Aca"
+	/*
+	public boolean dameAccion(Agente agente, Objeto objeto, Lugar lugar, int naccion) {
 		if(naccion == 0) {
 			Acciones.relocalizar(this, lugar);
 			return gestionJuego.GameManager.log(this, null, this.getLugar());
@@ -26,5 +46,5 @@ public abstract class Jugador extends Agente {
 		}
 		return gestionJuego.GameManager.log(this);
 	}
-
+	*/
 }

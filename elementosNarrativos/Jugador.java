e5a714693c0c7c25cd9680c961eaea6ff84c909a
Reitzz <75105818+Reitzz@users.noreply.github.com>
2021-01-10 21:22:14 +0100

    Jugador terminado (esperemos)

--- elementosNarrativos/Jugador.java
+++ elementosNarrativos/Jugador.java
@@ -8,43 +8,32 @@ public class Jugador extends Agente {
 	}
 	
 	public boolean dameAccion(BotonDeMoverse boton) {
-		
+		Acciones.relocalizar(this, boton.getLugar());
+		return gestionJuego.GameManager.log(this, null, this.getLugar());	
 	}
 	
 	public boolean dameAccion(BotonDeCoger boton) {
-		
+		if (boton.getNombre() != null){
+			Acciones.cogerObjeto(this, boton.getObjeto());
+			return gestionJuego.GameManager.log(this, boton.getObjeto(), this.getLugar());
+		}
+		Acciones.dejarObjeto(this);
+		return gestionJuego.GameManager.log(null, boton.getObjeto(), this.getLugar());
 	}
 	
 	public boolean dameAccion(BotonDePersona boton) {
-		//Aca
+		Acciones.pedirObjeto(boton.getAgente(), new Peticion(this, boton.getObjeto()));
+		return gestionJuego.GameManager.log(this.getPeticion().getAgente(), this.getPeticion().getObjeto(), this.getPeticion().getLugar());
 	}
 	
 	public boolean dameAccion(boolean dar) {
 		if(dar) {
-			//Aca
-		}
-	}
-	
-	//TODO mete cada Acciones.relocalizar y return gestionJugo.GameManager, donde pone "Aca"
-	/*
-	public boolean dameAccion(Agente agente, Objeto objeto, Lugar lugar, int naccion) {
-		if(naccion == 0) {
-			Acciones.relocalizar(this, lugar);
-			return gestionJuego.GameManager.log(this, null, this.getLugar());
-		}
-		if(naccion == 1) {
-			Acciones.cogerObjeto(this, this.getLugar().getObjeto());
-			return gestionJuego.GameManager.log(this, this.getObjeto(), this.getLugar());
-		}
-		if(naccion == 2) {
-			Acciones.pedirObjeto(agente, new Peticion(this, this.getObjetivo().getObjeto()));
-			return gestionJuego.GameManager.log(this.getPeticion().getAgente(), this.getPeticion().getObjeto(), this.getPeticion().getLugar()); 
-		}
-		if(naccion == 3) {
 			Acciones.darObjeto(this);
 			return gestionJuego.GameManager.log(this.getPeticion().getAgente(), this.getPeticion().getObjeto(), this.getPeticion().getLugar());
 		}
 		return gestionJuego.GameManager.log(this);
 	}
-	*/
+	public boolean dameAccion(){
+		return dameAccion(false);	
+	}
 }

a2c0b113f5bf6ff678327e043d47a15a8db8b523
Veranono29 <56341455+Veranono29@users.noreply.github.com>
2021-01-11 22:50:34 +0100

    Botones Porque Ekaitz quiere

--- gestionJuego/BotonDeMoverse.java
+++ gestionJuego/BotonDeMoverse.java
@@ -1,5 +1,21 @@
 package gestionJuego;
 
-public class BotonDeMoverse {
+import javax.swing.JButton;
+
+import elementosNarrativos.Lugar;
+
+public class BotonDeMoverse extends JButton{
+	private Lugar lugar;
+	
+	public BotonDeMoverse(Lugar lugar) {
+		super(lugar.getNombre());
+		this.lugar = lugar;
+	}
+	public Lugar getLugar() {
+		return lugar;
+	}
+	public void setLugar(Lugar lugar) {
+		this.lugar = lugar;
+	}
 
 }
--- gestionJuego/BotonDePersona.java
+++ gestionJuego/BotonDePersona.java
@@ -1,5 +1,20 @@
 package gestionJuego;
 
-public class BotonDePersona {
+import javax.swing.JButton;
+
+import elementosNarrativos.Agente;
+
+
+public class BotonDePersona extends JButton {
+
+Agente agente = null;
+
+	public BotonDePersona(Agente p) {
+		super(p.getNombre());
+		this.agente=p;
+	}
+	public Agente getAgente() {
+		return agente;
+	}
 
 }

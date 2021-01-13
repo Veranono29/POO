fbafd03aeefc0457aad5c1fe44d10f2f1d73f87b
Ekaitz723 <52826804+Ekaitz723@users.noreply.github.com>
2021-01-13 01:54:02 +0100

    le meto el string args y comento DataManager

--- gestionJuego/Main.java
+++ gestionJuego/Main.java
@@ -2,9 +2,9 @@ package gestionJuego;
 
 public class Main {
 	
-	public static void main() {
+	public static void main(String[] args) {
 		GameManager manejador = new GameManager();
 		manejador.main();
-		DataManager.main(manejador);
+		//DataManager.main(manejador);
 	}
 }
